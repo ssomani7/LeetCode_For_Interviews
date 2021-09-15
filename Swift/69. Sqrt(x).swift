d245a3d8c684c1a858994d2e098791c75dd41dd8
Firth Dev <shubhamvkamdi@gmail.com>
2021-09-14 17:27:31 -0700

    - Added 69. Sqrt(x)

---
+++ Swift/69. Sqrt(x).swift
@@ -0,0 +1,14 @@
+//
+//  69. Sqrt(x).swift
+//  
+//
+//  Created by Shubham Vinod Kamdi on 14/09/21.
+//
+
+import Foundation
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        print(sqrt(Double(x)))
+        return Int(sqrt(Double(x)))
+    }
+}
